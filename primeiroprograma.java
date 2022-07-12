270ba3c99140a7b58e34eb810b00a0dde0c7f97a
Giovani <108760391+Gio-53@users.noreply.github.com>
2022-07-12 16:39:13 -0300

    Add files via upload

---
+++ primeiroprograma.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+public class primeiroprograma {
+
+	public static void main(String[] args) {
+	
+		/*int a=2;
+		int b=3;
+		System.out.println("Hello world! " + (a+b));
+
+	}*/
+
+}
